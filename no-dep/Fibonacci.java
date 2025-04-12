fb40b1570223275617fb1176998ffde0e7df24b8
Heshan Padamsiri <hpheshan@gmail.com>
2025-04-12 18:30:35 +0530

    V2

--- no-dep/Fibonacci.java
+++ no-dep/Fibonacci.java
@@ -1,31 +1,30 @@
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Fibonacci {
     private static final int M = 30;
 
-    private static int fibonacci(int n) throws ExecutionException, InterruptedException {
+    private static CompletableFuture<Integer> fibonacci(int n, ExecutorService executorService)
+            throws ExecutionException, InterruptedException {
         if (n <= 0) {
-            return 0;
+            return CompletableFuture.completedFuture(0);
         }
         if (n == 1) {
-            return 1;
+            return CompletableFuture.completedFuture(1);
         }
 
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            Future<Integer> fib1 = executor.submit(() -> fibonacci(n - 1));
-            Future<Integer> fib2 = executor.submit(() -> fibonacci(n - 2));
+        CompletableFuture<Integer> fib1 = fibonacci(n - 1, executorService);
+        CompletableFuture<Integer> fib2 = fibonacci(n - 2, executorService);
 
-            int f1 = fib1.get();
-            int f2 = fib2.get();
-            return f1 + f2;
-        }
+        return fib1.thenCombine(fib2, (a, b) -> a + b);
     }
+
 
     public static void main(String[] args) {
         try {
-            int result = fibonacci(M);
+            int result = fibonacci(M, Executors.newVirtualThreadPerTaskExecutor()).get();
             if (result == 0) {
                 throw new RuntimeException("Unexpected result: computation was optimized away");
             }
